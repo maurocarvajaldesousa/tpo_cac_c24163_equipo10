2fc8181236bb27b72ca8faecbe5bad0e6311a0dc
maurocds <mauro.carvajaldesousa@gmail.com>
2024-07-02 16:37:01 -0300

    edito texto de popup al crear una obra

--- js/admin.js
+++ js/admin.js
@@ -2,16 +2,16 @@ const rutas = await getRutas();
 poblarSelectorRutas();
 
 async function getRutas() {
-  const response = await fetch("http://127.0.0.1:5000/api/rutas");
+  const response = await fetch('http://127.0.0.1:5000/api/rutas');
   const rutas = await response.json();
 
   return rutas;
 }
 
 function poblarSelectorRutas() {
-  const selectEl = document.querySelector("#rutas");
+  const selectEl = document.querySelector('#rutas');
   rutas.forEach((r) => {
-    const optionEl = document.createElement("option");
+    const optionEl = document.createElement('option');
     optionEl.value = r.id_ruta;
     optionEl.textContent = r.nombre;
 
@@ -23,16 +23,16 @@ const categorias = await getCategorias();
 poblarSelectorCategorias();
 
 async function getCategorias() {
-  const response = await fetch("http://127.0.0.1:5000/api/categorias");
+  const response = await fetch('http://127.0.0.1:5000/api/categorias');
   const categorias = await response.json();
 
   return categorias;
 }
 
 function poblarSelectorCategorias() {
-  const selectEl = document.querySelector("#categoria");
+  const selectEl = document.querySelector('#categoria');
   categorias.forEach((c) => {
-    const optionEl = document.createElement("option");
+    const optionEl = document.createElement('option');
     optionEl.value = c.id_categoria;
     optionEl.textContent = c.nombre;
 
@@ -40,24 +40,24 @@ function poblarSelectorCategorias() {
   });
 }
 
-const formEl = document.querySelector("#form-create-obra");
+const formEl = document.querySelector('#form-create-obra');
 
-formEl.addEventListener("submit", (event) => {
+formEl.addEventListener('submit', (event) => {
   event.preventDefault();
 
   const data = new FormData(formEl);
 
-  const descripcion = data.get("descripcion");
-  const anio = data.get("anio");
-  const partido = data.get("partido");
-  const localidad = data.get("localidad");
-  const latitud = +data.get("latitud");
-  const longitud = +data.get("longitud");
-  const gmaps = data.get("gmaps");
-  const imagen = data.get("imagen");
-  const ruta = +data.get("ruta");
-  const categoria = +data.get("categoria");
-  const estado = +data.get("estado");
+  const descripcion = data.get('descripcion');
+  const anio = data.get('anio');
+  const partido = data.get('partido');
+  const localidad = data.get('localidad');
+  const latitud = +data.get('latitud');
+  const longitud = +data.get('longitud');
+  const gmaps = data.get('gmaps');
+  const imagen = data.get('imagen');
+  const ruta = +data.get('ruta');
+  const categoria = +data.get('categoria');
+  const estado = +data.get('estado');
 
   const payload = {
     descripcion,
@@ -75,29 +75,29 @@ formEl.addEventListener("submit", (event) => {
 
   console.log(payload);
 
-  fetch("http://127.0.0.1:5000/api/obras", {
-    method: "POST",
+  fetch('http://127.0.0.1:5000/api/obras', {
+    method: 'POST',
     headers: {
-      "content-type": "application/json",
+      'content-type': 'application/json',
     },
     body: JSON.stringify(payload),
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log("Success:", data);
+      console.log('Success:', data);
       Swal.fire({
-        title: "Creaste una obra!",
-        text: "You clicked the button!",
-        icon: "success",
+        title: 'Creaste una obra!',
+        text: 'Obra creada exitosamente',
+        icon: 'success',
       });
       formEl.reset();
     })
     .catch((error) => {
-      console.error("Error:", error);
+      console.error('Error:', error);
       Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Algo salió mal",
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Algo salió mal',
       });
     });
 });
